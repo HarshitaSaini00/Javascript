fc615c1743095b7071fb9a0d553455704e180476
HarshitaSaini00 <harshitasaini283@gmail.com>
2025-07-06 17:49:13 +0000

    complete

---
+++ Basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) we are using nodejs,not browser
+
+console.log(3
+    +
+    3) //code readability should be high
+
+console.log("harshita");
+
+//read mdn for knowledge but mdn is not real , real is ECMA for javascript standerds
+
+let name = "harshita";
+let age = 21;
+let isLoggedIn = true;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true /false
+//null => standalone value
+//undefined => not yet value assign
+//symbol => unique
+
+//object
+
+console.log(typeof "harshita");//string
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
---
+++ Basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 45245;
+let accountEmail = "harshitasaini283@gmail.com";
+var accountPassword ="12345";
+accountCity = "delhi";
+
+// - these slash used for comment 
+//constant variable can't be changed 
+
+/* prefer not to use var 
+because of issue in block scope and functioncal scope
+*/
+let userstate ;//if we create any varibale and we do not assgin a value then it's give undefined value
+
+console.log("aacountId"); // used for one variable print
+console.log([accountId,accountEmail,accountPassword,accountCity]);//used for multiple variables to print
+
+
+
+
