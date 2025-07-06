6d77613f45e32ca3667b671277126ac95f3d3b0c
HarshitaSaini00 <harshitasaini283@gmail.com>
2025-07-06 18:13:16 +0000

    conversions of datatypes

---
+++ Basics/conversionoperations.js
@@ -0,0 +1,28 @@
+let score ="33"; //string
+console.log(typeof score);//string
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);//number
+console.log(valueInNumber);//Nan = not a number ,here we check what kind a number in this variable stored
+// we use score = null //value assign 0
+// score = undefined // Nan
+//score = true//1 ,false//0
+//score ="harshita"== Nan
+//"33" =>33
+//"33abc" =>NaN
+
+let isloggedIn =1;
+let booleanIsLoggedIn = Boolean(isloggedIn);
+console.log(booleanIsLoggedIn);//true
+
+//1 =>true
+//0 =>false
+//"" =>false
+//"harshita" =>true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);//33
+console.log(typeof stringNumber);//string
+
+
